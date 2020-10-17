d543957166334d7398bf75b4fbbf07314c497820
wanipriyanka4 <68581906+wanipriyanka4@users.noreply.github.com>
2020-10-17 22:53:48 +0530

    Java Assinment - Day4(all ass)

---
+++ Avenger.java
@@ -0,0 +1,29 @@
+ 
+ 
+ class Avenger
+{
+    int age,planet,power;
+    String name,weapon ,;
+    public void getDetails()
+	{
+        for(i=0;i<5;i++);
+		Syetem.out.println("Enter the Name");
+		System.out.println("Enetr the Age");
+		System.out.println("Enter the Power");
+		System.out.println("Enter the Weapon");
+		System.out.println("Enter the Planet");
+		
+		
+        
+     }
+    public void DisplayDetails()
+	{
+        System.out.print("age = "+Age + "  " + " Name = "+ name + " " + "power= "+ Power + "  " + " weapon = "+ Weapon + " " + planet = "+Planet ");
+        System.out.println();
+     }
+	 public statci void main(String args[])
+	{
+	 clrscr();
+	 Avenger  myObj1 = new Avenger();
+	
+}
---
+++ Oddnum.java
@@ -0,0 +1,14 @@
+ 
+class Oddnum
+
+ {
+ 
+  public static void main(Static args[])
+   arr = [1, 2, 3, 4, 5 ,6 ,7 ,8 ,9 ,10];   
+   
+  print("Elements of given array present on odd position: ");  
+
+  
+    for i in range(0, len(arr), 2):  
+    print(arr[i]); 
+ }	
